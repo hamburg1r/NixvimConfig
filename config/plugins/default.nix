decbcb1fb4f649f2531656e0678d6b2100c388c8
Hamburgir <shantnuprofid@gmail.com>
2026-05-05 02:05:45 +0530

    fixed explorer keybind and default.nix

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -18,16 +18,17 @@
     ./lz-n.nix
     ./markdown.nix
     ./mini.nix
+    ./neo-tree.nix
     ./nix.nix
     ./none-ls.nix
     ./oil.nix
     ./project-nvim.nix
     ./snacks.nix
-    ./utils.nix
     ./telescope.nix
     ./testing.nix
     ./treesitter.nix
     ./ui-core.nix
+    ./utils.nix
     ./which-key.nix
   ];
 
