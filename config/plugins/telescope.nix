2607a71e50ee70787b533a240e944d1dda6cccfd
Hamburgir <shantnuprofid@gmail.com>
2026-05-08 17:16:38 +0530

    Telescope changes
    * changed layout strategy to flex
    * sorting strategy to ascending
    * prompt on top

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -1,10 +1,24 @@
-{ config, lib, ... }:
-
-{
+{ config, lib, ... }: {
   plugins.telescope = {
     enable = true;
     settings = {
-      defaults = { };
+      defaults = {
+        border = true;
+        sorting_strategy = "ascending";
+        layout_strategy = "flex";
+        layout_config = {
+          horizontal = {
+            prompt_position = "top";
+          };
+          vertical = {
+            prompt_position = "top";
+          };
+        };
+      };
+      pickers = {
+        
+      };
+    };
     lazyLoad = {
       enable = true;
       settings = {
