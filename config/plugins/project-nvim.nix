bb955285b8a863a8883025bddd06271d4932939a
Hamburgir <shantnuprofid@gmail.com>
2026-05-08 17:18:42 +0530

    project-nvim changes
    * enabled autochdir
    * scope chdir: tab
    * allowed addition of different owners for repo

--- config/plugins/project-nvim.nix
+++ config/plugins/project-nvim.nix
@@ -2,5 +2,12 @@
   plugins.project-nvim = {
     enable = true;
     enableTelescope = true;
+    settings = {
+      enable_autochdir = true;
+      scope_chdir = "tab";
+      different_owners = {
+        allow = true;
+      };
+    };
   };
 }
