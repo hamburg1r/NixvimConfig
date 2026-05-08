1921bb85e68fafa6e7f7a6269c30925c0e2a2f2e
Hamburgir <shantnuprofid@gmail.com>
2026-05-08 22:26:59 +0530

    Fixed basic keymaps

--- config/keymaps/basic.nix
+++ config/keymaps/basic.nix
@@ -1,5 +1,5 @@
 { ... }: [
-  { key = "<Esc>"; action = "<cmd>nohlsearch<CR>"; plugin = "native"; }
-  { key = "<C-[>"; action = "<cmd>nohlsearch<CR>"; plugin = "native"; }
-  { key = "<Esc><Esc>"; action = "<C-\><C-n>"; options = { desc = "Exit terminal mode"; }; mode = [ "t" ]; plugin = "native"; }
+  { key = "<Esc>"; action = "<cmd>nohlsearch<CR>"; mode = [ "n" ]; plugin = "native"; }
+  { key = "<C-[>"; action = "<cmd>nohlsearch<CR>"; mode = [ "n" ]; plugin = "native"; }
+  { key = "<Esc><Esc>"; action = "<C-\\><C-n>"; options = { desc = "Exit terminal mode"; }; mode = [ "t" ]; plugin = "native"; }
 ]
